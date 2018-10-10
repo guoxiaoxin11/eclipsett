0e2b1c777bfdc9b869c7ef037fbb472ca4b23e48
Your Name <email@example.com>
2018-10-10 22:41:40 +0800

    2行

--- textGit/src/cn/itcast/text/demo2.java
+++ textGit/src/cn/itcast/text/demo2.java
@@ -6,7 +6,7 @@ public class demo2 {
 	public static void main(String[] args) {
 		int s=0;
 		for (int i = 0; i < 10; i++) {
-			System.out.println(s++);
+			System.out.println("aaaaa"+s++);
 		}
 	}
 
